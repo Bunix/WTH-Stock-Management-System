36ebe141fe9d3f6968fb6a91abace76eb4132d3f
faisol chehumar <faisol.chehumar@gmail.com>
2018-07-03 17:41:55 +0700

    computed function not return to view. Need fixed!

--- src/components/InStock.vue
+++ src/components/InStock.vue
@@ -20,7 +20,52 @@
       hide-actions
       :loading="tblLoading"
     >
+
+      <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
+      
+      <template slot="headerCell" slot-scope="props">
+        <v-tooltip bottom>
+          <span slot="activator">
+            {{ props.header.text }}
+          </span>
+          <span>
+            {{ props.header.text }}
+          </span>
+        </v-tooltip>
+      </template>
+
+      <template slot="items" slot-scope="props">
+        <td>
+          <v-checkbox
+            v-model="props.selected"
+            primary
+            hide-details
+          ></v-checkbox>
+        </td>
+        <!-- <td>{{ props }}</td> -->
+        <!-- <td>{{ props.item.detail.name }}</td> -->
+        <!-- <td v-if="props.item.printStatus === true">
+          <v-btn icon @click="toggleStatus(props.item)" class="green--text">
+            <v-icon>done</v-icon>
+          </v-btn>
+        </td>
+        <td v-else>
+          <v-btn icon @click="toggleStatus(props.item)" class="red--text">
+            <v-icon>clear</v-icon>
+          </v-btn>
+        </td>
+
+        <td>{{ props.item.customerInformation.name }}</td>
+        <td>{{ props.item.basicInformation.orderDate }}</td>
+        <td v-if="props.item.shipedDate === ''">-</td>
+        <td v-else>{{ props.item.shipedDate }}</td>
+        <td v-if="props.item.shelf === ''">-</td>
+        <td v-else>{{ props.item.shelf }}</td> -->
+      </template>
+
     </v-data-table>
+
+    <p>{{stockLists}}</p>
 
   </v-card>
 </template>
@@ -45,20 +90,36 @@ export default {
       ]
     }
   },
-  mounted() {
-    this.$nextTick(() => { // ES6 arrow function
-    /* eslint-disable */
-    console.log('mount')
-      this.inStockProducts = this.$store.getters.getInStockProductLists
-    })
-  },
+  // mounted() {
+  //   this.$nextTick(() => { // ES6 arrow function
+  //   /* eslint-disable */
+  //   console.log('mount')
+  //     this.inStockProducts = this.$store.getters.getInStockProductLists
+  //   })
+  // },
   watch: {
     inStockProducts: function () {
       /* eslint-disable */
       console.log('I\'m watching you')
+      console.log(this.inStockProducts.length)
       if(this.inStockProducts.length !== 0) {
         this.tblLoading = false
-        this.inStockProductsLocal = this.inStockProducts
+        // this.inStockProductsLocal = this.inStockProducts
+        // console.log(this.inStockProductsLocal)
+        // Object.keys(this.inStockProducts).forEach((sku) => {
+        //   // this.inStockProductsLocal.push(this.inStockProducts[sku])
+        //   let product = {
+        //     sku: this.inStockProducts[sku].sku,
+        //     detail: {
+        //       name: this.inStockProducts[sku].detail.name,
+        //       point: this.inStockProducts[sku].detail.point,
+        //       price: this.inStockProducts[sku].detail.price,
+        //       quantity: this.inStockProducts[sku].detail.quantity
+        //     }
+        //   }
+
+        //   this.inStockProductsLocal.push(product)
+        // })
       }
     }
   },
--- src/store/state.js
+++ src/store/state.js
@@ -4,5 +4,5 @@ export const state = {
   error: null,
   loading: false,
   barcodePrintLists: null,
-  inStockProductLists: null
+  inStockProductLists: []
 }
