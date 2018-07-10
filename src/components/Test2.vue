5157ece5e056a8394c8084313fce290ae3fe924e
faisol chehumar <faisol.chehumar@gmail.com>
2018-07-10 15:32:27 +0700

    Fixed Paid Date prop index problem

--- src/components/Test2.vue
+++ src/components/Test2.vue
@@ -31,6 +31,7 @@
     </v-card-title>
     <!-- ./ Search bar -->
 
+
     <v-toolbar flat color="white">
       <v-tooltip bottom>
         <span slot="activator">
@@ -118,12 +119,33 @@
           <td>{{ trimName(props.item.customer_name) }}</td>
           <td>{{ props.item.orderDate }}</td>
           <td v-if="typeof props.item.paidDate === 'undefined'">
+          <!-- <v-edit-dialog
+            :return-value.sync="props.item.setPaidDate"
+            large
+            lazy
+            persistent
+            @save="save"
+            @cancel="cancel"
+            @open="open"
+            @close="close"
+          >
+            <v-icon>event</v-icon>
+            <div>{{ props.item.setPaidDate }}</div>
+            <v-text-field
+              slot="input"
+              v-model="props.item.setPaidDate"
+              label="Edit"
+              single-line
+              counter
+              autofocus
+            ></v-text-field>
+          </v-edit-dialog> -->
             <template>
               <v-container grid-list-md>
                 <v-layout row wrap>
                   <v-flex xs12 lg6>
                     <v-menu
-                      ref="menu"
+                      ref="menu1"
                       :return-value.sync="props.item.setPaidDate"
                       :close-on-content-click="false"
                       :nudge-right="40"
@@ -134,19 +156,17 @@
                       max-width="290px"
                       min-width="290px"
                     >
-                      <v-icon color="blue darken-2" slot="activator">
+                      <v-icon v-show="!paidDateFormatted[props.index]" slot="activator">
                         event
                       </v-icon>
-                      <v-date-picker v-model="paidDate[props.index]" no-title>
-                        <v-spacer></v-spacer>
-                        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-                        <v-btn flat color="primary" @click="setPaidDate(props.item, props.index)">OK</v-btn>
-                      </v-date-picker>
+                      <v-date-picker v-model="paidDate[props.index]" no-title></v-date-picker>
                     </v-menu>
+                    <p>{{ paidDateFormatted[props.index] }}</p>
                   </v-flex>
                 </v-layout>
               </v-container>
             </template>
+            <!-- ./ Date picker -->
           </td>
           <td v-else>{{ props.item.paidDate }}</td>
           <td v-if="typeof props.item.shipedDate === 'undefined'">-</td>
@@ -167,10 +187,12 @@ const uuidv1 = require('uuid/v1')
 export default {/* eslint-disable */
   data: () => ({
     // isFilter: false,
-    menu: false,
     counter: 0,
     paidDate: [],
     paidDateFormatted: [],
+    date: null,
+    dateFormatted: null,
+    menu1: false,
     isScanMode: false,
     isAlert: false,
     toggle_exclusive: 0,
@@ -211,17 +233,18 @@ export default {/* eslint-disable */
         this.tblLoading = false
         this.orderListsTmp = this.orderLists
       }
+    },
+    paidDate() {
+      console.log('paid date')
+      if (this.paidDate !== null) {
+        this.paidDate.forEach((x, i) => {
+          this.paidDateFormatted[i] = this.formatDate(x)
+        })
+      }
     }
   },
   methods: {/* eslint-disable */
-    setPaidDate(item, index) {
-      console.log(item)
-      const [year, month, day] = this.paidDate[index].split('-')
-      item.paidDate = `${month}/${day}/${year}`
-      this.$store.dispatch('setPaidDate', [item])
-      this.menu = false
-    },
-    formatDate(date) {
+    formatDate (date) {
       if (!date) return null
 
       const [year, month, day] = date.split('-')
@@ -367,5 +390,3 @@ export default {/* eslint-disable */
     min-height: 100%;
   }
 </style>
-
-
